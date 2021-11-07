7b0ac3b026a9c6a0fd232cfbb0c9b6f62c4dc5b2
YuQuanOU <imyq.ou@gmail.com>
2021-11-07 21:42:36 +0800

    weather api

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// function reqListener() {
+//   console.log(this.responseText);
+// }
+
+/**
+ * get weather
+ */
+
+var oReq = new XMLHttpRequest();
+// oReq.addEventListener("load", reqListener);
+oReq.open("GET", "../js/weather.json", true);
+
+oReq.onload = function () {
+  let jsonResponse = JSON.parse(oReq.responseText);
+  let weather = jsonResponse.weather[0].main;
+  let temp = jsonResponse.main.temp;
+
+  let weather_status = document.getElementById("weather_status");
+
+  switch (weather) {
+    case "Clouds":
+      weather_status.innerHTML = '<i class="fas fa-cloud"></i>';
+      break;
+    case "Sun":
+      weather_status.innerHTML = '<i class="fas fa-cloud-rain fa-4x"></i>';
+      break;
+    default:
+      weather_status.innerHTML = "no";
+      break;
+  }
+
+  let weather_temp = document.getElementById("weather_temp");
+  weather_temp.innerText = temp.toFixed(0);
+
+};
+oReq.send();
