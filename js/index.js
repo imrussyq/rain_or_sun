4668108548debe4b402dcdd0fae5d342b9915749
YuQuanOU <imyq.ou@gmail.com>
2021-11-17 20:17:16 +0800

    update

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@
 
 var oReq = new XMLHttpRequest();
 // oReq.addEventListener("load", reqListener);
-oReq.open("GET", "./js/weather.json", true);
+oReq.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=0c441714fc50c1192aed587ecd4829cb&units=metric", true);
+// oReq.open("GET", "./js/weather.json", true);
 
 oReq.onload = function () {
   let jsonResponse = JSON.parse(oReq.responseText);
@@ -16,10 +17,11 @@ oReq.onload = function () {
   let temp = jsonResponse.main.temp;
 
   let weather_status = document.getElementById("weather_status");
+  console.log(weather);
 
   switch (weather) {
     case "clouds":
-      weather_status.innerHTML = '<i class="fas fa-cloud"></i>';
+      weather_status.innerHTML = '<i class="fas fa-cloud fa-4x"></i>';
       break;
     case "sun":
       weather_status.innerHTML = '<i class="fas fas fa-sun fa-4x"></i>';
@@ -28,7 +30,7 @@ oReq.onload = function () {
       weather_status.innerHTML = '<i class="fas fa-cloud-rain fa-4x"></i>';
       break;
     default:
-      weather_status.innerHTML = "no";
+      weather_status.innerHTML = '<i class="fas fa-cloud fa-4x"></i>';
       break;
   }
 
